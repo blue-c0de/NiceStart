e962ed2e7c80e2d1c5e0de4a0e6ff76421d07cc8
blue-c0de <cxsguillen@gmail.com>
2023-11-08 13:26:25 +0100

    Main Java Update

--- app/src/main/java/com/example/nicestart/MainActivity.java
+++ app/src/main/java/com/example/nicestart/MainActivity.java
@@ -11,6 +11,12 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
     public void openBoyTinder(View view){
         Intent intent = new Intent(MainActivity.this, BoyTinder.class);
         startActivity(intent);
