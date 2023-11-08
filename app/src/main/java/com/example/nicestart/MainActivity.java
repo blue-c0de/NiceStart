05f206c3e1c6a8c476b4d1a2f292e308b0f19897
blue-c0de <cxsguillen@gmail.com>
2023-11-08 23:08:23 +0100

    Main Landscape Update

--- app/src/main/java/com/example/nicestart/MainActivity.java
+++ app/src/main/java/com/example/nicestart/MainActivity.java
@@ -5,16 +5,29 @@ import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView chica, chico, ambos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chico = findViewById(R.id.imageView);
+        chica = findViewById(R.id.imageView2);
+        ambos = findViewById(R.id.imageView3);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            chico.setImageResource(R.drawable.man_small);
+            chica.setImageResource(R.drawable.woman_small);
+            ambos.setImageResource(R.drawable.dog_small);
+        }
     }
 
     public void openBoyTinder(View view){
